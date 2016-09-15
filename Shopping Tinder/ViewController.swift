8ce77937c0a070060b52e6c7f26e5263ddc0b9d2
Zeladgolan <elad@zimperium.com>
2016-09-14 19:23:23 -0500

    Initial Commit

---
+++ Shopping Tinder/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Shopping Tinder
+//
+//  Created by Elad Golan on 14/09/2016.
+//  Copyright © 2016 AsaEl. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Shopping TinderTests/Shopping_TinderTests.swift
@@ -0,0 +1,36 @@
+//
+//  Shopping_TinderTests.swift
+//  Shopping TinderTests
+//
+//  Created by Elad Golan on 14/09/2016.
+//  Copyright © 2016 AsaEl. All rights reserved.
+//
+
+import XCTest
+@testable import Shopping_Tinder
+
+class Shopping_TinderTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
