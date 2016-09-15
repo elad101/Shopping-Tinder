0667f4ed3fba3ea9bd5365c0d2be4908f1b09d4b
Zeladgolan <elad@zimperium.com>
2016-09-14 21:10:14 -0500

    first commit

--- Shopping Tinder/ViewController.swift
+++ Shopping Tinder/ViewController.swift
@@ -13,8 +13,19 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
+        var shoppingList = ["Apple", "Banana", "Pear"];
+        print(shoppingList);
+        weak var addProductBtn: UIButton!
+        
+        
+        }
 
+    @IBAction func addProduct(_ sender: UIButton) {
+        self.present(alert, animated: true, completion: nil)
+        
+    }
+    let alert = UIAlertController(title: "Alert", message: "Message", preferredStyle: UIAlertControllerStyle.alert)
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
