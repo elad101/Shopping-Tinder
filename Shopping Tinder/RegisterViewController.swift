d43c189a2a970d9fa7ba919d3f88d652d506f347
Zeladgolan <elad@zimperium.com>
2016-09-18 21:05:22 -0700

    Added Email validation

--- Shopping Tinder/RegisterViewController.swift
+++ Shopping Tinder/RegisterViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import Foundation
+
 
 class RegisterViewController: UIViewController,
 UITextFieldDelegate{
@@ -17,10 +19,27 @@ UITextFieldDelegate{
     @IBAction func actionTriggered(_ sender: AnyObject) {
         let message = "Yours Email: "+userEmailTextField.text! +
         "\n friend's Email: "+friendEmailTextField.text!
-        let alert = UIAlertController(title: "Oops!", message:message, preferredStyle: .alert)
-        alert.addAction(UIAlertAction(title: "OK", style: .default) { _ in })
-        self.present(alert, animated: true){}
+        if (isValidEmail(testStr: userEmailTextField.text!) && isValidEmail(testStr: friendEmailTextField.text!)){
+            
+            let alert = UIAlertController(title: "Oops!", message:message, preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default) { _ in })
+            self.present(alert, animated: true){}
+        }
+        else{
+            let alert = UIAlertController(title: "Oops!", message:"Email isn't valid", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default) { _ in })
+            self.present(alert, animated: true){}
+        }
+        
+        
 
+    }
+    func isValidEmail(testStr:String) -> Bool {
+        // print("validate calendar: \(testStr)")
+        let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}"
+        
+        let emailTest = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
+        return emailTest.evaluate(with: testStr)
     }
     
     func textFieldShouldReturn(_ textField: UITextField) -> Bool{
