95c0fe7eb006bac9be17b204aa1c6ce59941e8a0
asafpeleg <asaf.peleg@gmail.com>
2016-09-22 10:26:02 +0300

    Adding upload to S3 functionality, S3 framework

--- Shopping Tinder/ViewController.swift
+++ Shopping Tinder/ViewController.swift
@@ -7,6 +7,10 @@
 //
 
 import UIKit
+import AWSCore
+import AWSS3
+import AWSCognito
+
 
 class ViewController: UIViewController,
     UIImagePickerControllerDelegate,
@@ -75,6 +79,9 @@ class ViewController: UIViewController,
         let sendItemAction: UIAlertAction = UIAlertAction(title: "Send Item", style: .default) { action -> Void in
             // ********** Asaf Code for sending item goes here ********** 
             // This is your object String(describing: self.imagePicked.image
+            
+            self.uploadToS3(image: self.imagePicked.image!)
+            
             let alert = UIAlertController(title: "Shopping Tinder", message:"Item has been sent", preferredStyle: .alert)
             alert.addAction(UIAlertAction(title: "OK", style: .default) { _ in })
             self.present(alert, animated: true){}
@@ -95,6 +102,118 @@ class ViewController: UIViewController,
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    
+    func uploadToS3AndReturnTask(image : UIImage) -> AWSTask<AnyObject> {
+        
+        print("Uploading to S3 And returning a task")
+        
+        let credentialsProvider = AWSCognitoCredentialsProvider(regionType:.usEast1,
+                                                                identityPoolId:"us-east-1:3042c93b-519f-4bcf-b62d-bdf3fbbd50a2")
+        
+        let configuration = AWSServiceConfiguration(region:.usEast1, credentialsProvider:credentialsProvider)
+        
+        AWSServiceManager.default().defaultServiceConfiguration = configuration
+        
+        let date = NSDate()
+        let hashableString = NSString(format: "%f", date.timeIntervalSinceReferenceDate)
+        let imageData = UIImagePNGRepresentation(image)
+        let fileUrl = URL(fileURLWithPath: NSTemporaryDirectory()).appendingPathComponent((hashableString as String) + ".png") as NSURL
+        
+        do {
+            try imageData?.write(to: fileUrl as URL, options: NSData.WritingOptions.atomic)
+            
+        } catch {
+            print("Error writing file!")
+        }
+        
+        let uploadRequest = AWSS3TransferManagerUploadRequest()
+        uploadRequest?.bucket = "shoppingimages"
+        uploadRequest?.key = "abcdefgh" //Make this random
+        uploadRequest?.body = fileUrl as URL!
+        uploadRequest?.contentType = "image/png"
+        
+        // we will track progress through an
+        // AWSNetworkingUploadProgressBlock
+        uploadRequest?.uploadProgress =
+            {[unowned self](bytesSent:Int64,
+                totalBytesSent:Int64, totalBytesExpectedToSend:Int64) in
+                
+                DispatchQueue.main.sync(execute: { () -> Void in
+                    print(totalBytesSent)
+                    // you can have a loading stuff in here.
+                })
+        }
+        
+        
+        // now the upload request is set up we can
+        // create the transfermanger,
+        // the credentials are already set up in the app delegate
+        let transferManager:AWSS3TransferManager =
+            AWSS3TransferManager.default()
+        // start the upload
+        let task = transferManager.upload(uploadRequest)
+        
+        return task!
+    }
+    
+    
+    func uploadToS3(image : UIImage) {
+        
+        print("Uploading to S3")
+
+        let credentialsProvider = AWSCognitoCredentialsProvider(regionType:.usEast1,
+                                                                identityPoolId:"us-east-1:3042c93b-519f-4bcf-b62d-bdf3fbbd50a2")
+        
+        let configuration = AWSServiceConfiguration(region:.usEast1, credentialsProvider:credentialsProvider)
+        
+        AWSServiceManager.default().defaultServiceConfiguration = configuration
+        
+        let date = NSDate()
+        let hashableString = NSString(format: "%f", date.timeIntervalSinceReferenceDate)
+        let imageData = UIImagePNGRepresentation(image)
+        let fileUrl = URL(fileURLWithPath: NSTemporaryDirectory()).appendingPathComponent((hashableString as String) + ".png") as NSURL
+        
+        do {
+            try imageData?.write(to: fileUrl as URL, options: NSData.WritingOptions.atomic)
+
+        } catch {
+            print("Error writing file!")
+        }
+        
+        let uploadRequest = AWSS3TransferManagerUploadRequest()
+        uploadRequest?.bucket = "shoppingimages"
+        uploadRequest?.key = "abcdefgh" //Make this random
+        uploadRequest?.body = fileUrl as URL!
+        uploadRequest?.contentType = "image/png"
+        
+        // we will track progress through an
+        // AWSNetworkingUploadProgressBlock
+        uploadRequest?.uploadProgress =
+            {[unowned self](bytesSent:Int64,
+                totalBytesSent:Int64, totalBytesExpectedToSend:Int64) in
+                
+                DispatchQueue.main.sync(execute: { () -> Void in
+                    print(totalBytesSent)
+                    // you can have a loading stuff in here.
+                })
+        }
+        
+        
+        // now the upload request is set up we can
+        // create the transfermanger,
+        // the credentials are already set up in the app delegate
+        let transferManager:AWSS3TransferManager =
+            AWSS3TransferManager.default()
+        // start the upload
+        let task = transferManager.upload(uploadRequest)
+        task?.continue({ (task) -> Any? in
+            print("uploading...")
+        })
+        
+        print("Done with uploading to S3")
+        
     }
 }
 
